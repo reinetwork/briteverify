131f3c0cec13c0d884f0db3bbd93952573805c13
Pablo Nuñez <pablo@myhousedeals.com>
2014-07-03 13:16:01 -0300

    Closes #145 Adding last test

--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -2,24 +2,33 @@
 
 class ResponseTest extends PHPUnit_Framework_TestCase
 {
-    public function setUp()
-    {
-        parent::setUp();
-        $responseMock = $this->getMockBuilder('\GuzzleHttp\Message\Response')
-              ->setMethods(['json'])
-              ->getMock();
-        $this->class = new \REINetwork\BriteVerify\Response($responseMock);
-    }
-
-
     public function testClass()
     {
+      $responseMock = $this->getMockBuilder('\GuzzleHttp\Message\Response')
+          ->disableOriginalConstructor()
+          ->setMethods(['json'])
+          ->getMock();
+      $json = [
+          'address' => 'johndoe@briteverify.com',
+          'account' => 'johndoe',
+          'domain' => 'briteverify.com',
+          'status' => 'invalid',
+          'connected' => null,
+          'disposable' => 'false',
+          'role_address' => 'email@address.com',
+          'error_code' => 'email_account_invalid',
+          'error' => 'Email account invalid',
+          'duration' => 0.0000000000,
+      ];
+      $responseMock->expects($this->once())
+          ->method('json')
+          ->will($this->returnValue($json));
 
-
-       $this->assertSame(true, $this->class->isValid());
-       $this->assertSame(true, $this->class->isInvalid());
-       $this->assertSame(true, $this->class->isAcceptAll());
-       $this->assertSame(true, $this->class->isUnknown());
+      $this->class = new \REINetwork\BriteVerify\Response($responseMock);
+      $this->assertSame(false, $this->class->isValid());
+      $this->assertSame(true, $this->class->isInvalid());
+      $this->assertSame(false, $this->class->isAcceptAll());
+      $this->assertSame(false, $this->class->isUnknown());
     }
 
 }
