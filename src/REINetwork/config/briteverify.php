afd0ea2bdd7baa24996a02fe4a79bfd814411a23
Travis Miller <travis@reinetworklp.com>
2014-08-19 11:59:21 -0500

    Re-organized package so it will work within Laravels expectations. (Ref #10)

---
+++ src/REINetwork/config/briteverify.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | BriteVerify API Access Token
+    |--------------------------------------------------------------------------
+    |
+    | The API access token provided to you on creating a BriteVerify account.
+    |
+    */
+
+    'token' => null,
+
+);
